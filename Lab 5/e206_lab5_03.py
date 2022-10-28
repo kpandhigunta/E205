ae5d5ed6a0060ca7c2b76c9ff654d4b8506aaaa3
Kaanthi Pandhigunta <kpandhigunta@g.hmc.edu>
2022-10-28 10:45:28 -0700

    print removed

--- Lab 5/e206_lab5_03.py
+++ Lab 5/e206_lab5_03.py
@@ -21,7 +21,6 @@ def main():
         full_traj.extend(pts[0])
         start = waypoint
     desired_traj = full_traj
-    print(desired_traj)
 
     # Create the trajectory and tracking controller
     controller = PointTracker()
